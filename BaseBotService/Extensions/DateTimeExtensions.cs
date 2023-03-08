93e095220448d6dbdc83d531af6fe30b360df45c
Gordon Breuer <Gordon.Breuer@msg.group>
2023-03-08 21:18:28 +0100

    feat: added more data to the user-info command

---
+++ BaseBotService/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace BaseBotService.Extensions;
+public static class DateTimeExtensions
+{
+    public static long ToUnixTimestamp(this DateTime dateTime) => (long)(dateTime - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds;
+
+    public static long ToUnixTimestamp(this DateTimeOffset dateTimeOffset) => (long)(dateTimeOffset - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds;
+
+    public static string ToDiscordTimestamp(this DateTime dateTime) => $"<t:{dateTime.ToUnixTimestamp()}:f>";
+
+    public static string ToDiscordTimestamp(this DateTimeOffset dateTimeOffset) => $"<t:{dateTimeOffset.ToUnixTimestamp()}:f>";
+}
