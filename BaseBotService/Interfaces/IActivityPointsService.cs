9a014a1d5762bfe2865f76c0c1ea9cf407474848
Gordon Breuer <Gordon.Breuer@msg.group>
2023-03-08 19:17:27 +0100

    feat: Added interface members to query activity points and last active date for users in guilds

--- BaseBotService/Interfaces/IActivityPointsService.cs
+++ BaseBotService/Interfaces/IActivityPointsService.cs
@@ -1,7 +1,8 @@
-﻿using Discord;
-
-namespace BaseBotService.Interfaces;
+﻿namespace BaseBotService.Interfaces;
 public interface IActivityPointsService
 {
     Task AddActivityTick(ulong guildId, ulong userId);
+    uint GetActivityPoints(ulong guildId, ulong userId);
+    DateTime GetLastActive(ulong guildId, ulong userId);
+
 }
