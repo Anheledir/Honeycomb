20fa2be58682d356d570743020f786912bbc626c
Gordon Breuer <Gordon.Breuer@msg.group>
2023-03-15 02:05:14 +0100

    feat(utilities): Added method to generate a documentation of all slash-commands and user-commands as json

---
+++ BaseBotService/Utilities/GenerateDocumentation.cs
@@ -0,0 +1,57 @@
+﻿using Discord.Interactions;
+using System.Reflection;
+using System.Text.Json;
+
+namespace BaseBotService.Utilities;
+
+public static class DocumentationUtility
+{
+    public static string GenerateDocumentationJson()
+    {
+        var allTypes = Assembly.GetExecutingAssembly().GetTypes();
+        var interactionModules = allTypes.Where(t => t.IsSubclassOf(typeof(InteractionModuleBase<SocketInteractionContext>)));
+
+        var modulesDocumentation = new List<object>();
+
+        foreach (var module in interactionModules)
+        {
+            var methods = module.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly);
+            var commandsDocumentation = new List<object>();
+
+            foreach (var method in methods)
+            {
+                var slashCommandAttribute = method.GetCustomAttribute<SlashCommandAttribute>();
+                var userCommandAttribute = method.GetCustomAttribute<UserCommandAttribute>();
+
+                if (slashCommandAttribute is not null || userCommandAttribute is not null)
+                {
+                    var parameters = method.GetParameters();
+                    var parametersDocumentation = parameters.Select(p => new
+                    {
+                        p.Name,
+                        Type = p.ParameterType.Name,
+                        p.IsOptional,
+                        p.DefaultValue
+                    });
+
+                    commandsDocumentation.Add(new
+                    {
+                        CommandName = slashCommandAttribute?.Name ?? userCommandAttribute.Name,
+                        CommandType = slashCommandAttribute is not null ? "SlashCommand" : "UserCommand",
+                        Description = slashCommandAttribute?.Description ?? string.Empty,
+                        Parameters = parametersDocumentation
+                    });
+                }
+            }
+
+            modulesDocumentation.Add(new
+            {
+                ModuleName = module.Name,
+                Commands = commandsDocumentation
+            });
+        }
+
+        var jsonOptions = new JsonSerializerOptions { WriteIndented = true };
+        return JsonSerializer.Serialize(modulesDocumentation, jsonOptions);
+    }
+}
