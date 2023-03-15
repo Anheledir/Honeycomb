e8a99b0e09e88dd65ab7fc6841fa319aa021001c
Gordon Breuer <Gordon.Breuer@msg.group>
2023-03-15 18:53:24 +0100

    feat: Add corresponding tests for EmbedBuilderExtension
    - Add NUnit tests for EmbedBuilderExtension

---
+++ BaseBotServiceTests/Extensions/EmbedBuilderExtensionTests.cs
@@ -0,0 +1,40 @@
+﻿using BaseBotService.Extensions;
+using Discord;
+
+namespace BaseBotService.Tests.Extensions
+{
+    public class EmbedBuilderExtensionTests
+    {
+        [Test]
+        public void WithFieldIf_ConditionTrue_FieldIsAdded()
+        {
+            // Arrange
+            var embedBuilder = new EmbedBuilder();
+            var embedFieldBuilder = new EmbedFieldBuilder().WithName("Name").WithValue("Value");
+            bool condition = true;
+
+            // Act
+            embedBuilder.WithFieldIf(condition, embedFieldBuilder);
+
+            // Assert
+            embedBuilder.Fields.Count.ShouldBe(1);
+            embedBuilder.Fields[0].Name.ShouldBe("Name");
+            embedBuilder.Fields[0].Value.ShouldBe("Value");
+        }
+
+        [Test]
+        public void WithFieldIf_ConditionFalse_FieldIsNotAdded()
+        {
+            // Arrange
+            var embedBuilder = new EmbedBuilder();
+            var embedFieldBuilder = new EmbedFieldBuilder().WithName("Name").WithValue("Value");
+            bool condition = false;
+
+            // Act
+            embedBuilder.WithFieldIf(condition, embedFieldBuilder);
+
+            // Assert
+            embedBuilder.Fields.Count.ShouldBe(0);
+        }
+    }
+}
