74644b27c94c273c2f926019155b9a95f8dd712f
Gordon Breuer <Gordon.Breuer@msg.group>
2023-03-21 01:10:47 +0100

    feat: added handling of different component types incl. debug output

--- BaseBotService/Requests/MessageComponentHandler.cs
+++ BaseBotService/Requests/MessageComponentHandler.cs
@@ -1,31 +1,52 @@
 ﻿using BaseBotService.Messages;
+using Discord.WebSocket;
 
 namespace BaseBotService.Requests;
 public class MessageComponentHandler : IRequestHandler<MessageComponentRequest>
 {
     private readonly ILogger _logger;
-    private readonly InteractionService _interaction;
-    private readonly IServiceProvider _services;
 
-    public MessageComponentHandler(ILogger logger, InteractionService interaction, IServiceProvider services)
+    public MessageComponentHandler(ILogger logger)
     {
         _logger = logger;
-        _interaction = interaction;
-        _services = services;
     }
 
     async Task IRequestHandler<MessageComponentRequest>.Handle(MessageComponentRequest msg, CancellationToken cancellationToken)
     {
-        _logger.Debug("Message Component by {UserId} in {ChannelId} ({GuildId}) for '{@cmdData}'",
+        var component = msg.Context.Interaction as SocketMessageComponent;
+
+        _logger.Debug("Message Component by {UserId} in {ChannelId} ({GuildId}) for {@cmdData}",
             msg.Context.User.Id,
             msg.Context.Channel.Id,
             msg.Context.Guild?.Id,
-            msg.Context.Interaction.Data);
+            component.Data);
 
+        string message = string.Empty;
+        switch (component.Data.Type)
+        {
+            case ComponentType.ActionRow:
+                message = $"ActionRow {component.Data.CustomId}";
+                break;
+            case ComponentType.Button:
+                message = $"Button {component.Data.CustomId}";
+                break;
+            case ComponentType.SelectMenu:
+                message = $"Select {component.Data.CustomId}: {string.Join(',', component.Data.Values)}";
+                break;
+            case ComponentType.TextInput:
+                message = $"Input {component.Data.CustomId}: {component.Data.Value}";
+                break;
+            case ComponentType.ModalSubmit:
+                message = $"ModalSubmit {component.Data.CustomId}";
+                break;
+            default:
+                message = $"Unknown type {component.Data.Type}!";
+                break;
+        }
 
         if (msg.Context.Interaction.HasResponded)
-            await msg.Context.Interaction.FollowupAsync("Followup Done.");
+            await msg.Context.Interaction.FollowupAsync(message);
         else
-            await msg.Context.Interaction.RespondAsync("Response Done");
+            await msg.Context.Interaction.RespondAsync(message);
     }
 }
