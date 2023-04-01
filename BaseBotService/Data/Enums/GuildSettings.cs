9c2341efd503082aa196d7fe69e2c37cb8fd9589
Gordon Breuer <Gordon.Breuer@msg.group>
2023-04-02 00:26:47 +0200

    chore: added documentation for GuildSettings

--- BaseBotService/Data/Enums/GuildSettings.cs
+++ BaseBotService/Data/Enums/GuildSettings.cs
@@ -1,11 +1,33 @@
 ﻿namespace BaseBotService.Data.Enums;
 
+/// <summary>
+/// Represents the guild settings as a bitwise flag enumeration.
+/// </summary>
 [Flags]
 public enum GuildSettings
 {
+    /// <summary>
+    /// No settings enabled.
+    /// </summary>
     None = 0,
+
+    /// <summary>
+    /// Enables activity points tracking.
+    /// </summary>
     EnableActivityPoints = 1 << 0,
+
+    /// <summary>
+    /// Enables gender identity in user profiles.
+    /// </summary>
     EnableUserProfilesGenderIdentity = 1 << 1,
+
+    /// <summary>
+    /// Enables birthday information in user profiles.
+    /// </summary>
     EnableUserProfilesBirthday = 1 << 2,
+
+    /// <summary>
+    /// Enables public visibility for user profiles.
+    /// </summary>
     EnablePublicUserProfiles = 1 << 3,
 }
