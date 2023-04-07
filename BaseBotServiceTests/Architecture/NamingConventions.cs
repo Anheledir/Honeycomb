c2bbda41026af6f4585ee233ca18740e6054b6ae
Gordon Breuer <Gordon.Breuer@msg.group>
2023-04-08 01:07:18 +0200

    feat: Added unit first tests for compliance with architecture with NetArchTest

---
+++ BaseBotServiceTests/Architecture/NamingConventions.cs
@@ -0,0 +1,35 @@
+﻿using NetArchTest.Rules;
+
+namespace BaseBotService.Tests.Architecture;
+
+[TestFixture]
+internal class NamingConventions
+{
+    [Test]
+    public void AllInterfaces_ShouldStartWIthPrefix_I()
+    {
+        var result = Types.InCurrentDomain()
+            .That()
+            .AreInterfaces()
+            .Should()
+            .HaveNameStartingWith("I")
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue(result.GetFailingTypes());
+    }
+
+    [Test]
+    public void AllClasses_WithSuffixBase_ShouldBeAbstract()
+    {
+        var result = Types.InCurrentDomain()
+            .That()
+            .ResideInNamespaceStartingWith("BaseBotService")
+            .And()
+            .HaveNameEndingWith("Base")
+            .Should()
+            .BeAbstract()
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue(result.GetFailingTypes());
+    }
+}
